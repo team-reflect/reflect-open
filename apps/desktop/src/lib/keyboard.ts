7766470f218862524ae4bd5e1506315e3dcf115e
ocavue <ocavue@gmail.com>
2026-07-27 15:48:09 +1000

    wip

--- apps/desktop/src/lib/keyboard.ts
+++ apps/desktop/src/lib/keyboard.ts
@@ -4,16 +4,18 @@ let timer : ReturnType<typeof setTimeout> | undefined
 // Workaround for a bug in WebKit where the isComposing property is reset to false event when the IME is still composing.
 // https://bugs.webkit.org/show_bug.cgi?id=311717
 export function isKeyboardEventComposing(event: KeyboardEvent): boolean {
-  if (timer) {
-    clearTimeout(timer)
-    timer = undefined
-  }
+
 
   if (event.isComposing) {
+    if (timer) {
+      clearTimeout(timer)
+      timer = undefined
+    }
     isComposing = true
   } else if (isComposing) {
     timer = setTimeout(() => {
       isComposing = false
+      timer = undefined
     }, 40)
   }
 
