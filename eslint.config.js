97a3460097027ef49bf1a37497793c85478671a4
ocavue <ocavue@gmail.com>
2026-08-06 16:31:57 +1000

    eslint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import { defineESLintConfig } from '@ocavue/eslint-config'
+
+export default defineESLintConfig(
+  {
+    react: {
+      version: '19.2',
+      reactCompiler: true,
+      files: ['**/*.tsx'],
+    },
+    markdown: false,
+  },
+)
