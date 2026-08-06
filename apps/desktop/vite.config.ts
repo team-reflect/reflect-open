7338150e226641a8f4d2f6eec96792d43d18f064
ocavue <ocavue@gmail.com>
2026-08-06 15:41:56 +1000

    fix type

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -65,13 +65,11 @@ export default defineConfig(  ({
     port: 1420,
     strictPort: true,
     host: host || false,
-    hmr: host
-      ? {
-          protocol: 'ws',
-          host,
-          port: 1421,
-        }
-      : undefined,
+    ...(host ? {
+        protocol: 'ws',
+        host,
+        port: 1421,
+    } : {}),
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
       ignored: ['**/src-tauri/**'],
